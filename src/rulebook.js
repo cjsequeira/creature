23d45fe12b55ad82f366ba98dedeb7cf8f84d6d8
arrogantfool <63619139+arrogantfool@users.noreply.github.com>
2021-02-12 18:58:39 -0500

    Implementation of rulebook bintree

--- src/rulebook.js
+++ src/rulebook.js
@@ -3,38 +3,53 @@
 // ****** Simulation rulebook ******
 // Inspired by: https://ericlippert.com/2015/05/11/wizards-and-warriors-part-five/
 
-
-// *** Rulebook functions
-// general rulebook resolver
-// returns physicalType
-export const ResolveRules = (physicalType = {}) => {
-    return (
-        // is it a creatureType?
-        (physicalType.hasOwnProperty('conds'))
-            // yes: resolve rules for creatureType
-            ? ResolveRules_CreatureType(physicalType)
-
-            // no: return given physicalType
-            : physicalType
-    )
-};
-
-// rulebook resolver for creatureType
-// returns creatureType
-const ResolveRules_CreatureType = (creatureType = { conds: null }) => {
-    return (
-        // glucose and neuro in range?
-        ((creatureType.conds.glucose > 0.0) && (creatureType.conds.neuro < 100.0))
-            // yes: return given creatureType
-            ? creatureType
-
-            // no: return creatureType with behavior of 'frozen'
-            : {
+// *** The rulebook
+const ruleBook = {
+    // is it a creatureType? (i.e. does it have 'conds' property?)
+    func: (physicalType) => physicalType.hasOwnProperty('conds'),
+    // yes:
+    yes: {
+        // are glucose and neuro in range?
+        func: (creatureType) => ((creatureType.conds.glucose > 0.0) &&
+            (creatureType.conds.neuro < 100.0)),
+        // yes: return given creatureType
+        yes: {
+            func: (creatureType) => creatureType
+        },
+        // no: return creatureType with behavior of "frozen"
+        no: {
+            func: (creatureType) => ({
                 ...creatureType,
                 conds: {
                     ...creatureType.conds,
                     behavior: 'frozen'
                 }
-            }
-    )
-}
+            })
+        }
+    },
+    // no: return given physicalType
+    no: {
+        func: (physicalType) => physicalType
+    }
+};
+
+
+// *** Rulebook functions
+// general rulebook resolver
+// returns physicalType
+export const ResolveRules = (arg = {}, node = ruleBook) => {
+    console.log(node.func.toString());
+    let funcResult = node.func(arg);
+
+    switch (funcResult) {
+        case true:
+            console.log('got true');
+            return ResolveRules(arg, node.yes);
+        case false:
+            console.log('got false')
+            return ResolveRules(arg, node.no);
+        default:
+            console.log(funcResult);
+            return funcResult;
+    }
+};
