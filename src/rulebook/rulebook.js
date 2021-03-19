2d367a0d9c314f2e721f0f95333ba02e7d1eece5
arrogantfool <63619139+arrogantfool@users.noreply.github.com>
2021-03-18 20:31:47 -0400

    Move rulebook test nodes to separate file

--- src/rulebook/rulebook.js
+++ src/rulebook/rulebook.js
@@ -24,18 +24,23 @@
 import { event_replacePhysType } from './event_creators.js';
 
 import {
-    EVENT_UPDATE_ALL_PHYSTYPES,
-    EVENT_REPLACE_CREATURETYPE,
-    EVENT_REPLACE_PHYSTYPE,
-    WORLD_TOUCH_DISTANCE,
-} from '../const_vals.js';
+    isBehaviorRequestIdling,
+    isBehaviorRequestSleeping,
+    isBehaviorRequestWandering,
+    isCreatureTouchingFood,
+    isEventReplaceCreatureType,
+    isEventReplacePhysType,
+    isEventUpdateAllPhysTypes,
+    isFoodTouchedByCreature,
+    isFoodType,
+    isGlucoseNeuroInRange,
+    isSimpleCreature,
+} from './test_nodes.js';
 
 import {
     excludeRange,
     orTests,
 } from '../utils.js';
-
-import { actAsSimpleCreature } from '../phystypes/simple_creature.js';
 
 import {
     action_replacePhysType,
@@ -47,7 +52,6 @@ import {
 } from '../reduxlike/action_creators.js';
 
 import {
-    getPhysTypeAct,
     getPhysTypeCond,
     getPhysTypeCondsObj,
     getPhysTypeID,
@@ -68,8 +72,6 @@ import {
     rand_unwrapRandType,
 } from '../sim/seeded_rand.js';
 
-import { actAsFood } from '../phystypes/food_type.js';
-
 
 // *** Creature behavior strings
 // REFACTOR
@@ -81,105 +83,6 @@ const behaviorStrings = {
     frozen: "is frozen! Brrrr....."
 };
 
-
-// *** Rulebook test nodes
-const isBehaviorRequestIdling = {
-    name: 'Requesting behavior: idling?',
-    testFunc: (_) => (rand_eventType) =>
-        getPhysTypeCond(rand_eventType.value.physType)('behavior_request') === 'idling',
-};
-
-const isBehaviorRequestSleeping = {
-    name: 'Requested behavior: sleeping?',
-    testFunc: (_) => (rand_eventType) =>
-        getPhysTypeCond(rand_eventType.value.physType)('behavior_request') === 'sleeping',
-};
-
-const isBehaviorRequestWandering = {
-    name: 'Requesting behavior: wandering?',
-    testFunc: (_) => (rand_eventType) =>
-        getPhysTypeCond(rand_eventType.value.physType)('behavior_request') === 'wandering',
-};
-
-const isFoodType = {
-    name: 'Is foodType?',
-    testFunc: (_) => (rand_eventType) =>
-        getPhysTypeAct(rand_eventType.value.physType) === actAsFood,
-};
-
-const isFoodTouchedByCreature = {
-    name: 'Is this food being touched by creature?',
-    testFunc: (storeType) => (rand_eventType) =>
-        // get physType store
-        getPhysTypeStore(storeType)
-            // keep only simple creatures
-            .filter(
-                (ptToTest1) => getPhysTypeAct(ptToTest1) === actAsSimpleCreature
-            )
-
-            // keep only creatures closer than a given distance from this foodType
-            .filter((ptToTest2) => Math.sqrt(
-                Math.pow(getPhysTypeCond(ptToTest2)('x') -
-                    getPhysTypeCond(rand_eventType.value.physType)('x'), 2.0) +
-                Math.pow(getPhysTypeCond(ptToTest2)('y') -
-                    getPhysTypeCond(rand_eventType.value.physType)('y'), 2.0)
-            ) < WORLD_TOUCH_DISTANCE)
-
-            // any creatures remaining closer than a given distance?
-            .length > 0,
-};
-
-const isSimpleCreature = {
-    name: 'Is creatureType?',
-    testFunc: (_) => (rand_eventType) =>
-        getPhysTypeAct(rand_eventType.value.physType) === actAsSimpleCreature,
-};
-
-// REFACTOR IDEA: Create an event where the food being touched by the creature can be tagged in, for efficiencies
-// GOAL: Avoid scanning food to see what's being eaten - just send the specific food objects
-const isCreatureTouchingFood = {
-    name: 'Is this creature touching food?',
-    testFunc: (storeType) => (rand_eventType) =>
-        // get physType store
-        getPhysTypeStore(storeType)
-            // keep only food
-            .filter(
-                (ptToTest1) => getPhysTypeAct(ptToTest1) === actAsFood
-            )
-
-            // keep only food closer than a given distance from this creatureType
-            .filter((ptToTest2) => Math.sqrt(
-                Math.pow(getPhysTypeCond(ptToTest2)('x') -
-                    getPhysTypeCond(rand_eventType.value.physType)('x'), 2.0) +
-                Math.pow(getPhysTypeCond(ptToTest2)('y') -
-                    getPhysTypeCond(rand_eventType.value.physType)('y'), 2.0)
-            ) < WORLD_TOUCH_DISTANCE)
-
-            // any food remaining closer than a given distance?
-            .length > 0,
-};
-
-const isEventUpdateAllPhysTypes = {
-    name: 'Is event of type EVENT_UPDATE_ALL_PHYSTYPES?',
-    testFunc: (_) => (rand_eventType) => rand_eventType.value.type === EVENT_UPDATE_ALL_PHYSTYPES,
-};
-
-const isEventReplaceCreatureType = {
-    name: 'Is event of type EVENT_REPLACE_CREATURETYPE?',
-    testFunc: (_) => (rand_eventType) => rand_eventType.value.type === EVENT_REPLACE_CREATURETYPE,
-};
-
-const isEventReplacePhysType = {
-    name: 'Is event of type EVENT_REPLACE_PHYSTYPE?',
-    testFunc: (_) => (rand_eventType) => rand_eventType.value.type === EVENT_REPLACE_PHYSTYPE,
-};
-
-const isGlucoseNeuroInRange = {
-    name: 'Glucose and neuro in range?',
-    testFunc: (_) => (rand_eventType) =>
-        (getPhysTypeCond(rand_eventType.value.physType)('glucose') > 0.0) &&
-        (getPhysTypeCond(rand_eventType.value.physType)('neuro') < 100.0),
-};
 
 
 // *** Rulebook leaf nodes
